823ae5d0d824394964f60adc89db08f0fedf0d36
Jayrille <126861221+Jayrille@users.noreply.github.com>
2025-03-10 09:31:19 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import TodoList from './TodoList';
+import './App.css';
+import '@fontsource/press-start-2p';
 
 function App() {
   const [count, setCount] = useState(0)
