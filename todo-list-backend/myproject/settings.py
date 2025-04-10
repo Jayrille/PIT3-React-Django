d92659293c8cb8cafe3acbba894d870b36f4aa70
Jayrille <tubera.jayrille2004@gmail.com>
2025-04-10 14:18:00 +0800

    Token Authentication

--- todo-list-backend/myproject/settings.py
+++ todo-list-backend/myproject/settings.py
@@ -50,12 +50,16 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 ]
 
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
